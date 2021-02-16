51a006474bd39a35714dc81dbef19b2a3cf771af
Arocmd9 <akanderuben17@gmail.com>
2021-02-16 23:33:01 +0000

    nice auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
